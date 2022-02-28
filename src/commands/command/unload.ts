3f44993d707582927576724db639e2c80a5306ce
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2022-02-28 09:07:34 +0300

    fix: apply correct type names

--- src/commands/command/unload.ts
+++ src/commands/command/unload.ts
@@ -3,14 +3,14 @@ import { BaseTypeExtractReturnTs } from '../../types/base/Base';
 import { CommandType } from '../../types/Command';
 import { BaseCommand, BaseCommandOptions } from '../base/BaseCommand';
 
-export type CommandDisableCommandOptions<ctxT extends {}> = Omit<BaseCommandOptions<ctxT, []>, 'arguments'>;
+export type CommandUnloadCommandOptions<ctxT extends {}> = Omit<BaseCommandOptions<ctxT, []>, 'arguments'>;
 
-export type CommandDisableCommandArgTs<ctxT extends {}> = [
+export type CommandUnloadCommandArgTs<ctxT extends {}> = [
   CommandType<ctxT, never, false>,
 ];
 
-export class CommandDisableCommand<ctxT extends {}> extends BaseCommand<ctxT, CommandDisableCommandArgTs<ctxT>> {
-  public constructor (manager: CommanderoManager<ctxT>, options: CommandDisableCommandOptions<ctxT>) {
+export class CommandUnloadCommand<ctxT extends {}> extends BaseCommand<ctxT, CommandUnloadCommandArgTs<ctxT>> {
+  public constructor (manager: CommanderoManager<ctxT>, options: CommandUnloadCommandOptions<ctxT>) {
     super(manager, {
       isGuarded: true,
       isHidden: true,
@@ -21,7 +21,7 @@ export class CommandDisableCommand<ctxT extends {}> extends BaseCommand<ctxT, Co
     });
   }
 
-  public async run ([arg1]: BaseTypeExtractReturnTs<ctxT, CommandDisableCommandArgTs<ctxT>>, rawArgs: [], context: ctxT): Promise<void> {
+  public async run ([arg1]: BaseTypeExtractReturnTs<ctxT, CommandUnloadCommandArgTs<ctxT>>, rawArgs: [], context: ctxT): Promise<void> {
     if (arg1 instanceof BaseCommand) {
       this.manager.commands.unregister(arg1);
       await arg1.destroy();
