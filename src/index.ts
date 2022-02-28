ae6476e4ca67741b209733d5ad4fabd640e5b9e4
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2022-02-28 15:08:56 +0300

    feat: add exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+export * from './arguments';
+export * from './commands';
+export * from './locales';
+export * from './throttle';
+export * from './types';
+export * from './util';
+
 export * from './CommandGroupManager';
 export * from './CommandManager';
 export * from './CommanderoManager';
