3a73a41b3d298510ca603d9859a540e2897555b1
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2022-03-05 20:28:15 +0300

    refactor: include message as first argument everywhere

--- src/CommanderoManager.ts
+++ src/CommanderoManager.ts
@@ -64,7 +64,7 @@ export class CommanderoManager<ctxT extends {}> extends EventEmitter {
   public async handleMessage (message: string, context: ctxT): Promise<void> {
     await this.inhibitors.checkThrow(message, context);
 
-    const prefix = await this.handleMessagePrefix(context);
+    const prefix = await this.handleMessagePrefix(message, context);
     const regexp = await this.handlePrefixRegExp(prefix);
     const data = regexp.exec(message);
     if (data == null) return;
@@ -77,46 +77,39 @@ export class CommanderoManager<ctxT extends {}> extends EventEmitter {
     await command.handleMessage(strArgs, context);
   }
 
-  public async handleMessageError (context: ctxT, err: Error): Promise<void> {
+  public async handleMessageError (message: string, context: ctxT, err: Error): Promise<void> {
     try {
       if (err instanceof UserError) {
-        await this.handleMessageUserError(context, err);
+        await this.handleMessageUserError(message, context, err);
       } else {
         throw err;
       }
     } catch (err) {
-      await this.handleMessageThrowableError(context, err as Error);
+      await this.handleMessageThrowableError(message, context, err as Error);
     }
   }
 
-  public async handleMessageThrowableError (context: ctxT, err: Error): Promise<void> {
-    if (err instanceof UserError) {
-      await this.handleMessageUserError(context, err);
-    }
-    throw err;
+  public async handleMessageThrowableError (message: string, context: ctxT, err: Error): Promise<void> {
+    console.log(err);
   }
 
-  public async handleMessageUserError (context: ctxT, err: UserError): Promise<string> {
-    const locale = await this.handleMessageLocale(context);
-    const message = this.localization.translateUnsafe(locale, err.message) ?? this.localization.translateUnsafe(locale, 'error.unknown');
-    if (message == null) throw new LocalizationKeyError(err.message);
-    return message;
+  public async handleMessageUserError (message: string, context: ctxT, err: UserError): Promise<string> {
+    const locale = await this.handleMessageLocale(message, context);
+    const msg = this.localization.translateUnsafe(locale, err.message) ?? this.localization.translateUnsafe(locale, 'error.unknown');
+    if (msg == null) throw new LocalizationKeyError(err.message);
+    return msg;
   }
 
-  public async handleError (err: unknown): Promise<void> {
-    console.error(err);
-  }
-
-  public async handleMessageLocale (context: ctxT): Promise<string> {
+  public async handleMessageLocale (message: string, context: ctxT): Promise<string> {
     return 'en';
   }
 
-  public async handleMessagePrefix (context: ctxT): Promise<string | undefined> {
+  public async handleMessagePrefix (message: string, context: ctxT): Promise<string | undefined> {
     return this.commandPrefix;
   }
 
   @Memoize({})
   public async handlePrefixRegExp (prefix: string | undefined): Promise<RegExp> {
-    return new RegExp(`^${escapeRegExp(prefix ?? '')}(?:\\s+(.+))?$`);
+    return new RegExp(`^${escapeRegExp(prefix ?? '')}\\s*(.+)$`, 'i');
   }
 }
